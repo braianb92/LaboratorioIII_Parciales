c385cf6b4d016854f72e841b253e703a44a2e367
Braian Baldino <baldino.bab@hotmail.com>
2020-07-17 21:47:16 -0300

    parcial 2

---
+++ Parcial 2/src/index.js
@@ -0,0 +1,23 @@
+window.onload = function(){
+    // const tablaMascota = [{'nombre':'Firu','raza':'Cocker'},
+    //                       {'nombre':'Pepe','raza':'Siberiano'},
+    //                       {'nombre':'Chimi','raza':'Pekines'},
+    //                       {'nombre':'Pipi','raza':'Bulldog'},
+    //                       {'nombre':'Michi','cantidad vidas':'9'},
+    //                       {'nombre':'Pili','cantidad vidas':'2'},
+    //                       {'nombre':'Lumi','cantidad vidas':'3'},
+    //                       {'nombre':'Pepi','cantidad vidas':'5'},
+    //                       {'nombre':'Aguilon','tipo':'Rapiña'},
+    //                       {'nombre':'Puchi','tipo':'Rapiña'},
+    //                       {'nombre':'Polly','tipo':'Loro'},
+    //                       {'nombre':'Galo','tipo':'Loro'}]
+
+    // var tablaMascotas = new HtmlComponents.Tabla('tablaMascota');   
+    // tablaMascotas.createTable(tablaMascota,['nombre','atributo']);
+    // tablaMascotas.setTableClass('tabla');
+    // console.log('Headers Tabla Mascotas:',tablaMascotas.getTableHeader());
+    // console.log('Datos Tabla Mascotas:',tablaMascotas.getTableData());
+}
+
+
+
